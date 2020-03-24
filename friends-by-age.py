930b9b5b406806c52322eac6c9bb0906e243ad06
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2020-03-24 14:29:47 -0600

    Add files via upload

---
+++ friends-by-age.py
@@ -0,0 +1,22 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+def sortTuple(tuple):
+    return(sorted(tuple, key = lambda x: float(x[1]), reverse = True))
+
+lines = sc.textFile("file:////home/miguel/Documents/SparkCourse/Key-VAlue-RDDs/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
+results = averagesByAge.collect()
+results_sorted = sortTuple(results)
+for result in results_sorted:
+    print(result)
